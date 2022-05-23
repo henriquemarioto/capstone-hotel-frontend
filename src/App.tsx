b7eaafbc7275b06745df0c74efe1a3716e6f6126
Alexandre Araujo <alexandre01393@gmail.com>
2022-05-23 13:18:30 -0300

    feat: service provider done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import GlobalStyle from "./styles/global";
 import Routes from "./routes/routes";
+import {Toaster} from 'react-hot-toast'
+import { useService } from "./providers/Service";
+import { useEffect } from "react";
 
 function App() {
+
+  const {services, service, getAllServices, getOneService} = useService()
+
+  useEffect(() => {
+    getAllServices()
+  },[])
+  
+  console.log(services)
+
   return (
     <>
+      <Toaster />
       <GlobalStyle />
       <Routes />
     </>
--- src/providers/Service/index.tsx
+++ src/providers/Service/index.tsx
@@ -1,32 +1,107 @@
 import { createContext, ReactNode, useContext, useState } from "react";
-
+import apiHotel from "../../services/apiHotel";
+import { toast } from "react-hot-toast";
 
 interface ServiceProps {
-    children: ReactNode
+  children: ReactNode;
+}
+
+interface Service {
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+  status: boolean;
+  created_at: Date;
+  updated_at: Date;
+}
+
+interface UpdatedProps {
+  name?: string;
+  price?: number;
+  description?: string;
 }
 
 interface ServiceContextData {
-
+  services: Service[];
+  service: Service[];
+  getAllServices: () => Promise<void>;
+  getOneService: (id: string) => Promise<void>;
+  updatedService: (data: UpdatedProps, id: string) => Promise<void>;
+  disableService: (id: string) => Promise<void>;
 }
 
-const ServiceContext = createContext<ServiceContextData>({} as ServiceContextData)
+const ServiceContext = createContext<ServiceContextData>(
+  {} as ServiceContextData
+);
 
 const useService = () => {
-    const context = useContext(ServiceContext)
+  const context = useContext(ServiceContext);
 
-    if (!context) {
-        throw new Error ('teste')
-    }
+  if (!context) {
+    throw new Error("teste");
+  }
 
-    return context
-}
+  return context;
+};
 
-const ServiceProvider = ({children}: ServiceProps) => {
-    return (
-        <ServiceContext.Provider value={{}}>
-            {children}
-        </ServiceContext.Provider>
-    )
-}
+const ServiceProvider = ({ children }: ServiceProps) => {
+  const [services, setServices] = useState<Service[]>([]);
+  const [service, setService] = useState<Service[]>([]);
 
-export {ServiceProvider, useService}
+  const getAllServices = async () => {
+    const { data } = await apiHotel.get("services", {
+      headers: {
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc0FkbWluIjp0cnVlLCJpYXQiOjE2NTMzMjE0NjgsImV4cCI6MTY1MzU4MDY2OCwic3ViIjoiODZjMGUzY2UtYmViNS00MTNkLThmY2EtNzQwOTA0YzA5MzdjIn0.9Q3HHFjXPcFzDNhmP8AzSmqlg_JaBRlNGpmXLwfMcfQ`,
+      },
+    });
+    console.log(data)
+    setServices(data);
+  };
+
+  const getOneService = async (id: string) => {
+    const { data } = await apiHotel.get(`services/${id}`, {
+      headers: {
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc0FkbWluIjp0cnVlLCJpYXQiOjE2NTMzMjE0NjgsImV4cCI6MTY1MzU4MDY2OCwic3ViIjoiODZjMGUzY2UtYmViNS00MTNkLThmY2EtNzQwOTA0YzA5MzdjIn0.9Q3HHFjXPcFzDNhmP8AzSmqlg_JaBRlNGpmXLwfMcfQ`,
+      },
+    });
+    setService(data);
+  };
+
+  const updatedService = async (data: UpdatedProps, id: string) => {
+    await apiHotel
+      .patch(`services/${id}`, data, {
+        headers: {
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc0FkbWluIjp0cnVlLCJpYXQiOjE2NTMzMjE0NjgsImV4cCI6MTY1MzU4MDY2OCwic3ViIjoiODZjMGUzY2UtYmViNS00MTNkLThmY2EtNzQwOTA0YzA5MzdjIn0.9Q3HHFjXPcFzDNhmP8AzSmqlg_JaBRlNGpmXLwfMcfQ`,
+        },
+      })
+      .then((_) => getAllServices());
+  };
+
+  const disableService = async (id: string) => {
+    await apiHotel
+      .delete(`services/${id}`, {
+        headers: {
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc0FkbWluIjp0cnVlLCJpYXQiOjE2NTMzMjE0NjgsImV4cCI6MTY1MzU4MDY2OCwic3ViIjoiODZjMGUzY2UtYmViNS00MTNkLThmY2EtNzQwOTA0YzA5MzdjIn0.9Q3HHFjXPcFzDNhmP8AzSmqlg_JaBRlNGpmXLwfMcfQ`,
+        },
+      })
+      .then((_) => toast.success("Service disable"));
+  };
+
+  return (
+    <ServiceContext.Provider
+      value={{
+        getAllServices,
+        getOneService,
+        updatedService,
+        disableService,
+        services,
+        service,
+      }}
+    >
+      {children}
+    </ServiceContext.Provider>
+  );
+};
+
+export { ServiceProvider, useService };
