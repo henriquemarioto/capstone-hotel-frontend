d325a34defa31c76e5fb0ea123bc026dc4f11e0d
bryantorresribeiro <bryant0rr3sribeiro@gmail.com>
2022-05-23 13:57:31 -0500

    build: Provider clients

---
+++ src/providers/clients/index.tsx
@@ -0,0 +1,79 @@
+import {createContext, ReactNode, useEffect, useState} from "react"
+
+import apiHotel from "../../services/apiHotel"
+//import {useLogin} from "../Login"
+
+interface Bedroom {
+  id: string
+  number: string
+  floor: string
+  capacity: number
+  availability: boolean
+  createdAt: Date
+  updatedAt: Date
+  status: boolean
+  clients: []
+}
+
+type BedroomInput = Pick<
+  Bedroom,
+  "number" | "floor" | "capacity" | "availability"
+>
+
+interface UpdatedProps {
+  capacity?: number
+  availability?: boolean
+}
+
+interface BedroomProps {
+  children: ReactNode
+}
+
+interface BedroomContextData {
+  bedrooms: Bedroom[]
+  bedroom: Bedroom
+  getAllBedroom: () => Promise<void>
+  getAllServices: () => Promise<void>
+  getOneService: (id: string) => Promise<void>
+  updatedBedroom: (data: UpdatedProps, id: string) => Promise<void>
+  disableBedroom: (id: string) => Promise<void>
+  filterByStatus: (status: boolean) => Promise<void>
+}
+
+const BedroomContext = createContext<BedroomContextData>(
+  {} as BedroomContextData
+)
+
+export const BedroomProvider = ({children}: BedroomProps) => {
+  const [bedrooms, setBedrooms] = useState<Bedroom[]>([])
+  const [bedroom, setBedroom] = useState<Bedroom>()
+  const {token} = useLogin()
+
+  const createBedroom = async (bedroomInput: BedroomInput) => {
+    const {data} = await apiHotel.post("/bedrooms", bedroomInput, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    setBedrooms([...bedrooms, data])
+  }
+
+  const getAllBedrooms = async () => {
+    const {data} = await apiHotel.get("bedrooms", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    setBedrooms(data)
+  }
+
+  const getOneBedroom = async (id: string) => {
+    const {data} = await apiHotel.get(`bedrooms/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    setBedroom(data)
+  }
+}
