fb83d9d01dd28549af6cd219cf3455aa45a48322
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-05-24 10:35:28 -0300

    Feat: add bedrooms card component and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import GlobalStyle from "./styles/global";
-import Routes from "./routes/routes";
-import {Toaster} from 'react-hot-toast'
-import { useService } from "./providers/Service";
-import { useEffect } from "react";
-
+import GlobalStyle from "./styles/global"
+import Routes from "./routes/routes"
+import { Toaster } from "react-hot-toast"
+import { useService } from "./providers/Service"
+import { useEffect } from "react"
 
 function App() {
-
   return (
     <>
       <Toaster />
