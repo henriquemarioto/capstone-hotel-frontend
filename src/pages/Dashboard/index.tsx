2ec34258a46d34bf4f0a8c5a5b3d0ae3598e8b48
bryantorresribeiro <bryant0rr3sribeiro@gmail.com>
2022-05-25 09:50:44 -0500

    feat: style dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,24 +1,29 @@
-import { Container } from './style'
+import { Menu, Container } from './style'
 import GlobalStyle from "./style";
 import icon_people from "./img/icon-people.png"
 import icon from "./img/icon.png"
-
+import background from "./img/background.png"
+import Header from '../../components/Header';
 const Dashboard = () => {
     return(
         <>
             <GlobalStyle/>
-            <Container>
-                <div>
-                    <span>+</span>
-                    <p>New client</p>
-                    <img src={icon} alt="icon"/>
-                </div>
-                <div>
-                    <span>+</span>
-                    <p>New contractt</p>
-                    <img src={icon_people} alt="icon"/>
-                </div>
-            </Container>
+            <Header/>
+            <Menu>
+                <img className='background' src={background} alt=""/>
+                <Container>
+                    <div>
+                        <span>+</span>
+                        <p>New client</p>
+                        <img src={icon_people} alt="icon"/>
+                    </div>
+                    <div>
+                        <span>+</span>
+                        <p>New contract</p>
+                        <img src={icon} alt="icon"/>
+                    </div>
+                </Container>
+            </Menu>
         </>
     );
 }
--- src/pages/Dashboard/style.ts
+++ src/pages/Dashboard/style.ts
@@ -3,27 +3,71 @@ import styled, { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
 body {
    display: flex;
-   margin-top: 40px;
+   align-items: center;
    justify-content: center;
+   .background{
+      display: none;
+      margin-top: 60px;
+      margin-left: -20px;
+    }
+   @media (min-width: 930px) {
+    .background{
+      display: flex;
+    }
+  }
+    @media (min-width:  1400px) {
+    .background{
+      display: flex;
+      height: 600px;
+    }
+  }
 }
+
+`
+export const Menu = styled.section`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
 `
 export const Container = styled.section`
+  margin-top: 200px;
   transition: 0.4s;
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  justify-content: space-evenly;
   width: 280px;
   height: 165px;
   background-color: var(--beige-1);
   border-radius: 5px;
   div{
       display: flex;
+      align-items: center;
+  }
+  p{
+    margin: 0 10px;
+    font-weight: 500;
   }
   span{
-      padding: 5px;
+      transition: 0.4s;
+      padding: 10px;
       background-color: var(--white-0);
       border-radius: 5px;
+      cursor: pointer;
+      margin-left: 20px;
   }
+  span:hover{
+    transition: 0.4s;
+    background-color: var(--beige-dark);
+  }
+  @media (min-width: 560px) {
+   width: 350px;
+   height: 200px;
+   font-size: 20px;
+   span{
+     padding: 15px;
+   }
+  }
+
 `
 export default GlobalStyle
+  
