a21af72b8e908950e55467a120298858701cc7e5
NehamaMandelbaum <nehama1836@gmail.com>
2022-05-25 14:24:36 -0300

    styles: styles in service page done

--- src/pages/services/index.tsx
+++ src/pages/services/index.tsx
@@ -2,18 +2,83 @@ import Button from "../../components/Button";
 import Header from "../../components/Header";
 import Input from "../../components/Input";
 import ServiceCard from "../../components/ServiceCard";
+import {
+  Container,
+  InputContainer,
+  HeadingContainer,
+  CardsContainer,
+} from "./styles";
+
+import {FiSearch} from "react-icons/fi";
 
 export const Services = () => {
   return (
     <>
       <Header />
-      <Input label={"Input de teste"} />
-      <Button>Botão teste</Button>
-      <ServiceCard
-        name="Café da manhã"
-        description="Café da manhã gourmet com diversas opções."
-        price={20}
-      />
+      <Container>
+        <HeadingContainer>
+          <h1>Services</h1>
+          <InputContainer>
+            <Input label={"Choose a service"} />
+            <Button>
+              <FiSearch />
+            </Button>
+          </InputContainer>
+        </HeadingContainer>
+
+        <CardsContainer>
+          <ServiceCard
+            name="Café da manhã"
+            description="Café da manhã gourmet com diversas opções."
+            price={20}
+          />
+          <ServiceCard
+            name="Café da manhã"
+            description="Café da manhã gourmet com diversas opções."
+            price={20}
+          />
+          <ServiceCard
+            name="Café da manhã"
+            description="Café da manhã gourmet com diversas opções."
+            price={20}
+          />
+          <ServiceCard
+            name="Café da manhã"
+            description="Café da manhã gourmet com diversas opções."
+            price={20}
+          />
+          <ServiceCard
+            name="Café da manhã"
+            description="Café da manhã gourmet com diversas opções."
+            price={20}
+          />
+          <ServiceCard
+            name="Café da manhã"
+            description="Café da manhã gourmet com diversas opções."
+            price={20}
+          />
+          <ServiceCard
+            name="Café da manhã"
+            description="Café da manhã gourmet com diversas opções."
+            price={20}
+          />
+          <ServiceCard
+            name="Café da manhã"
+            description="Café da manhã gourmet com diversas opções."
+            price={20}
+          />
+          <ServiceCard
+            name="Café da manhã"
+            description="Café da manhã gourmet com diversas opções."
+            price={20}
+          />
+          <ServiceCard
+            name="Café da manhã"
+            description="Café da manhã gourmet com diversas opções."
+            price={20}
+          />
+        </CardsContainer>
+      </Container>
     </>
   );
 };
