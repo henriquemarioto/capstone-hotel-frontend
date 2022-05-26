69ad097da6a891a1d091350d8823ea2a69087ce5
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-05-26 14:34:43 -0300

    Update: added filter in service provider

--- src/pages/services/index.tsx
+++ src/pages/services/index.tsx
@@ -1,6 +1,5 @@
 import Button from "../../components/Button"
-import Header from "../../components/Header"
-import Input from "../../components/Input"
+import { Input } from "../../components/Input"
 import ServiceCard from "../../components/ServiceCard"
 import {
   Container,
@@ -12,61 +11,44 @@ import {
 import { FiSearch } from "react-icons/fi"
 import { useService } from "../../providers/Service"
 import { useEffect, useState } from "react"
+import { useLogin } from "../../providers/Login"
 
 export const Services = () => {
-  const { services, getAllServices } = useService()
+  const { services, getAllServices, filter, filteredService } = useService()
+  const { token } = useLogin()
 
   const [search, setSearch] = useState("")
-  const [servicesList, setServicesList] = useState(services)
 
   useEffect(() => {
-    getAllServices()
+    getAllServices(token)
   }, [])
 
-  const filter = (search: string) => {
-    const services = [...servicesList]
-    const filteredServices = services.filter(
-      (service) => String(service.name).toLocaleLowerCase() === search
-    )
-
-    if (filteredServices.length > 0) {
-      return setServicesList(filteredServices)
-    } else {
-      return setServicesList(services)
-    }
-  }
-
   return (
-    <>
-      <Header />
-      <Container>
-        <HeadingContainer>
-          <h1>Services</h1>
-          <InputContainer>
-            {/* <Input label={"Choose a service"} /> */}
-            <input
-              type="text"
-              onChange={(event) => setSearch(event.target.value)}
-            />
-            <Button onClick={() => filter(search)}>
-              <FiSearch />
-            </Button>
-          </InputContainer>
-        </HeadingContainer>
+    <Container>
+      <HeadingContainer>
+        <h1>Services</h1>
+        <InputContainer>
+          {/* <Input label={"Choose a service"} /> */}
+          <Input
+            title="Search"
+            type="text"
+            onChange={(event) => setSearch(event.target.value)}
+          />
+          <Button onClick={() => filter(search)}>
+            <FiSearch />
+          </Button>
+        </InputContainer>
+      </HeadingContainer>
 
-        <CardsContainer>
-          {services.map((service) => {
-            return (
-              <ServiceCard
-                name={service.name}
-                description={service.description}
-                price={service.price}
-                key={service.id}
-              />
-            )
-          })}
-        </CardsContainer>
-      </Container>
-    </>
+      <CardsContainer>
+        {filteredService.length > 0
+          ? filteredService.map((service) => (
+              <ServiceCard key={service.id} service={service} />
+            ))
+          : services.map((service) => (
+              <ServiceCard key={service.id} service={service} />
+            ))}
+      </CardsContainer>
+    </Container>
   )
 }
