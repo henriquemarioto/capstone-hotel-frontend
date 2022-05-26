b14916c25c5456d56a6e4642c3162e75ffd05017
Alexandre Araujo <alexandre01393@gmail.com>
2022-05-26 10:36:10 -0300

    fix: fixing get clients

--- src/pages/Clients/index.tsx
+++ src/pages/Clients/index.tsx
@@ -1,101 +1,39 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { FaSearch, FaArrowLeft } from "react-icons/fa"
 import ClientCard from "../../components/clientCard"
+import { useClients } from "../../providers/clients"
+import { useLogin } from "../../providers/Login"
 import { Container, DivInitial, DivSearch, InputDiv, Ul } from "./style"
 
-const arrayTeste = [
-  {
-    name: "Cliente 1",
-    cpf: "555.444.333-22",
-    cellphone: "988887777",
-    birthDate: "13/06/2001",
-    bedroom: "1",
-  },
-  {
-    name: "Cliente 2",
-    cpf: "555.444.333-22",
-    cellphone: "988887777",
-    birthDate: "13/06/2001",
-    bedroom: "1",
-  },
-  {
-    name: "Cliente 3",
-    cpf: "555.444.333-22",
-    cellphone: "988887777",
-    birthDate: "13/06/2001",
-    bedroom: "1",
-  },
-  {
-    name: "Cliente 4",
-    cpf: "555.444.333-22",
-    cellphone: "988887777",
-    birthDate: "13/06/2001",
-    bedroom: "1",
-  },
-  {
-    name: "Cliente 5",
-    cpf: "555.444.333-22",
-    cellphone: "988887777",
-    birthDate: "13/06/2001",
-    bedroom: "1",
-  },
-  {
-    name: "Cliente 6",
-    cpf: "555.444.333-22",
-    cellphone: "988887777",
-    birthDate: "13/06/2001",
-    bedroom: "1",
-  },
-  {
-    name: "Cliente 7",
-    cpf: "555.444.333-22",
-    cellphone: "988887777",
-    birthDate: "13/06/2001",
-    bedroom: "1",
-  },
-  {
-    name: "Cliente 8",
-    cpf: "555.444.333-22",
-    cellphone: "988887777",
-    birthDate: "13/06/2001",
-    bedroom: "1",
-  },
-  {
-    name: "Cliente 9",
-    cpf: "555.444.333-22",
-    cellphone: "988887777",
-    birthDate: "13/06/2001",
-    bedroom: "1",
-  },
-  {
-    name: "Cliente 10",
-    cpf: "555.444.333-22",
-    cellphone: "988887777",
-    birthDate: "13/06/2001",
-    bedroom: "1",
-  },
-]
 
 const Clients = () => {
-  const [clientsList, setClientsList] = useState(arrayTeste)
-  const [search, setSearch] = useState("")
+  const {getAllClients, clients} = useClients()
+  const {token} = useLogin()
+  
+  // const [clientsList, setClientsList] = useState(arrayTeste)
+  // const [search, setSearch] = useState("")
 
-  const filter = (search: string) => {
-    const clients = [...arrayTeste]
-    const filteredClients = clients.filter((client) => {
-      return (
-        String(client.name).toLocaleLowerCase() === search ||
-        String(client.cpf) === search ||
-        String(client.bedroom) === search
-      )
-    })
+  // const filter = (search: string) => {
+  //   const clients = [...arrayTeste]
+  //   const filteredClients = clients.filter((client) => {
+  //     return (
+  //       String(client.name).toLocaleLowerCase() === search ||
+  //       String(client.cpf) === search ||
+  //       String(client.bedroom) === search
+  //     )
+  //   })
 
-    if (filteredClients.length > 0) {
-      return setClientsList(filteredClients)
-    } else {
-      return setClientsList(arrayTeste)
-    }
-  }
+  //   if (filteredClients.length > 0) {
+  //     return setClientsList(filteredClients)
+  //   } else {
+  //     return setClientsList(arrayTeste)
+  //   }
+  // }
+
+  useEffect(() => {
+    getAllClients(token)
+  },[])
+
   return (
     <Container>
       <DivInitial>
@@ -110,17 +48,16 @@ const Clients = () => {
           <label>Search</label>
           <input
             type="text"
-            onChange={(event) => setSearch(event.target.value)}
           />
         </InputDiv>
-        <button onClick={() => filter(search)}>
+        <button>
           <FaSearch />
         </button>
       </DivSearch>
       <main>
         <Ul>
-          {clientsList.map((teste) => (
-            <ClientCard client={teste} />
+          {clients.map((client) => (
+            <ClientCard client={client} />
           ))}
         </Ul>
       </main>
