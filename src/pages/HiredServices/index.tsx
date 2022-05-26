0f64c1e0d32b83dd9f9a3cd8ea75cd597e92a04d
Alexandre Araujo <alexandre01393@gmail.com>
2022-05-26 10:39:06 -0300

    fix: fixing get hired services

--- src/pages/HiredServices/index.tsx
+++ src/pages/HiredServices/index.tsx
@@ -1,108 +1,38 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { FaArrowLeft, FaSearch } from "react-icons/fa"
 import HiredServiceCard from "../../components/HiredServiceCard"
+import { useHiredService } from "../../providers/HiredService"
+import { useLogin } from "../../providers/Login"
 import { Container, DivInitial, DivSearch, InputDiv, Ul } from "./style"
 
-const arrayTeste = [
-  {
-    id: 1,
-    bedroom: "5",
-    start_date: "15/05/2020",
-    end_date: "20/05/2020",
-    paid: false,
-  },
-  {
-    id: 2,
-    bedroom: "5",
-    start_date: "15/05/2020",
-    end_date: "20/05/2020",
-    paid: false,
-  },
-  {
-    id: 3,
-    bedroom: "5",
-    start_date: "15/05/2020",
-    end_date: "20/05/2020",
-    paid: false,
-  },
-  {
-    id: 4,
-    bedroom: "5",
-    start_date: "15/05/2020",
-    end_date: "20/05/2020",
-    paid: false,
-  },
-  {
-    id: 5,
-    bedroom: "5",
-    start_date: "15/05/2020",
-    end_date: "20/05/2020",
-    paid: false,
-  },
-  {
-    id: 6,
-    bedroom: "5",
-    start_date: "15/05/2020",
-    end_date: "20/05/2020",
-    paid: true,
-  },
-  {
-    id: 7,
-    bedroom: "5",
-    start_date: "15/05/2020",
-    end_date: "20/05/2020",
-    paid: false,
-  },
-  {
-    id: 8,
-    bedroom: "5",
-    start_date: "15/05/2020",
-    end_date: "20/05/2020",
-    paid: true,
-  },
-  {
-    id: 9,
-    bedroom: "5",
-    start_date: "15/05/2020",
-    end_date: "20/05/2020",
-    paid: false,
-  },
-  {
-    id: 10,
-    bedroom: "5",
-    start_date: "15/05/2020",
-    end_date: "20/05/2020",
-    paid: true,
-  },
-  {
-    id: 11,
-    bedroom: "5",
-    start_date: "15/05/2020",
-    end_date: "20/05/2020",
-    paid: true,
-  },
-]
-
 const HiredService = () => {
-  const [search, setSearch] = useState("")
-  const [contractsList, setContractsList] = useState(arrayTeste)
 
-  const filter = (search: string) => {
-    const contracts = [...contractsList]
-    const filteredContracts = contracts.filter((contract) => {
-      return (
-        String(contract.bedroom).toLocaleLowerCase() === search ||
-        String(contract.id).toLocaleLowerCase() === search ||
-        String(contract.paid).toLocaleLowerCase() === search
-      )
-    })
+  const {getAllHiredServices, hiredServices} = useHiredService()
+  const {token} = useLogin()
+  // const [search, setSearch] = useState("")
+  // const [contractsList, setContractsList] = useState(arrayTeste)
 
-    if (filteredContracts.length > 0) {
-      return setContractsList(filteredContracts)
-    } else {
-      return setContractsList(arrayTeste)
-    }
-  }
+  // const filter = (search: string) => {
+  //   const contracts = [...contractsList]
+  //   const filteredContracts = contracts.filter((contract) => {
+  //     return (
+  //       String(contract.bedroom).toLocaleLowerCase() === search ||
+  //       String(contract.id).toLocaleLowerCase() === search ||
+  //       String(contract.paid).toLocaleLowerCase() === search
+  //     )
+  //   })
+
+  //   if (filteredContracts.length > 0) {
+  //     return setContractsList(filteredContracts)
+  //   } else {
+  //     return setContractsList(arrayTeste)
+  //   }
+  // }
+
+  useEffect(() => {
+    getAllHiredServices(token)
+  },[])
+
   return (
     <Container>
       <DivInitial>
@@ -117,17 +47,16 @@ const HiredService = () => {
           <label>Find a contract</label>
           <input
             type="text"
-            onChange={(event) => setSearch(event.target.value)}
           />
         </InputDiv>
-        <button onClick={() => filter(search)}>
+        <button >
           <FaSearch />
         </button>
       </DivSearch>
       <main>
         <Ul>
-          {contractsList.map((teste) => (
-            <HiredServiceCard hiredService={teste} />
+          {hiredServices.map((hiredService) => (
+            <HiredServiceCard hiredService={hiredService} />
           ))}
         </Ul>
       </main>
