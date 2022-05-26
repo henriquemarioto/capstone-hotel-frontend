18e1fd9d299b2eeccd276cde975db4a4b23ca2a6
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-05-26 14:25:19 -0300

    Update: added filter in hired services

--- src/pages/HiredServices/index.tsx
+++ src/pages/HiredServices/index.tsx
@@ -1,108 +1,20 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { FaArrowLeft, FaSearch } from "react-icons/fa"
 import HiredServiceCard from "../../components/HiredServiceCard"
+import { useHiredService } from "../../providers/HiredService"
+import { useLogin } from "../../providers/Login"
 import { Container, DivInitial, DivSearch, InputDiv, Ul } from "./style"
 
-const arrayTeste = [
-  {
-    id: 1,
-    bedroom: "5",
-    start_date: "15/05/2020",
-    end_date: "20/05/2020",
-    paid: false,
-  },
-  {
-    id: 2,
-    bedroom: "5",
-    start_date: "15/05/2020",
-    end_date: "20/05/2020",
-    paid: false,
-  },
-  {
-    id: 3,
-    bedroom: "5",
-    start_date: "15/05/2020",
-    end_date: "20/05/2020",
-    paid: false,
-  },
-  {
-    id: 4,
-    bedroom: "5",
-    start_date: "15/05/2020",
-    end_date: "20/05/2020",
-    paid: false,
-  },
-  {
-    id: 5,
-    bedroom: "5",
-    start_date: "15/05/2020",
-    end_date: "20/05/2020",
-    paid: false,
-  },
-  {
-    id: 6,
-    bedroom: "5",
-    start_date: "15/05/2020",
-    end_date: "20/05/2020",
-    paid: true,
-  },
-  {
-    id: 7,
-    bedroom: "5",
-    start_date: "15/05/2020",
-    end_date: "20/05/2020",
-    paid: false,
-  },
-  {
-    id: 8,
-    bedroom: "5",
-    start_date: "15/05/2020",
-    end_date: "20/05/2020",
-    paid: true,
-  },
-  {
-    id: 9,
-    bedroom: "5",
-    start_date: "15/05/2020",
-    end_date: "20/05/2020",
-    paid: false,
-  },
-  {
-    id: 10,
-    bedroom: "5",
-    start_date: "15/05/2020",
-    end_date: "20/05/2020",
-    paid: true,
-  },
-  {
-    id: 11,
-    bedroom: "5",
-    start_date: "15/05/2020",
-    end_date: "20/05/2020",
-    paid: true,
-  },
-]
-
 const HiredService = () => {
+  const { getAllHiredServices, hiredServices, filter, filteredHired } =
+    useHiredService()
+  const { token } = useLogin()
   const [search, setSearch] = useState("")
-  const [contractsList, setContractsList] = useState(arrayTeste)
 
-  const filter = (search: string) => {
-    const contracts = [...contractsList]
-    const filteredContracts = contracts.filter((contract) => {
-      return (
-        String(contract.bedroom).toLocaleLowerCase() === search ||
-        String(contract.id).toLocaleLowerCase() === search ||
-        String(contract.paid).toLocaleLowerCase() === search
-      )
-    })
+  useEffect(() => {
+    getAllHiredServices(token)
+  }, [])
 
-    if (filteredContracts.length > 0) {
-      return setContractsList(filteredContracts)
-    } else {
-      return setContractsList(arrayTeste)
-    }
-  }
   return (
     <Container>
       <DivInitial>
@@ -126,9 +38,19 @@ const HiredService = () => {
       </DivSearch>
       <main>
         <Ul>
-          {contractsList.map((teste) => (
-            <HiredServiceCard hiredService={teste} />
-          ))}
+          {filteredHired.length > 0
+            ? filteredHired.map((hiredService) => (
+                <HiredServiceCard
+                  key={hiredService.id}
+                  hiredService={hiredService}
+                />
+              ))
+            : hiredServices.map((hiredService) => (
+                <HiredServiceCard
+                  key={hiredService.id}
+                  hiredService={hiredService}
+                />
+              ))}
         </Ul>
       </main>
     </Container>
